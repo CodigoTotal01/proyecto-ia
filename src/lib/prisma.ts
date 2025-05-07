366b9ca5ca6b275f9326dc2fc7f6107feefdceb4
CodigoTotal01 <palaciostarrillochristian@gmail.com>
2025-05-07 17:31:02 -0500

    S6-A: Base de datos y autenticacion

---
+++ src/lib/prisma.ts
@@ -0,0 +1,15 @@
+
+
+import { PrismaClient } from '@prisma/client'
+
+// eslint-disable-next-line no-var
+declare global {
+  var prisma: PrismaClient | undefined
+}
+
+export const client =
+  globalThis.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prisma = client
+}
